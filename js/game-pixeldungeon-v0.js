4203b77b2a25202a137a1670fd6684b3ca9149a7
brindle <mrbrindlestudio@gmail.com>
2015-07-11 18:38:23 +0100

    Basic sudo code for JS v0 done. Basic structure of game stage html v0 done. Basic CSS for stage and game elements done. Next: basic variables and functions to set stats for 1 player and 1 monster. Run a single battle. Update gamefield.

---
+++ js/game-pixeldungeon-v0.js
@@ -0,0 +1,30 @@
+// create player
+// set player stats
+// name=player1, lvl=1, hp=10 atk=1
+// setup first stage
+  // change stage id to stage 1
+// create monster level1
+// show player1 actions menu
+// get player choice
+// run choice (attack / defend /inventory)
+// decide what monster will do
+// compare player atk to monster def
+// lower monster hp from remainder if
+// repeat until monster or player is dead
+// if monster dies, play effect and remove
+// show confirmation player won
+// display loot won
+// add loot to player inventory and next button
+// display stage-select
+// player choose route A / B etc
+// send option to stage select
+// check if option requires key
+// check if player has key in inventory - if so allow option.
+// SET CURRENT STAGE
+
+// ? can a player go back or only forward?
+// ? how do they win. limited dungeon?
+// ? difficulty curve by room or floor?
+// ? inventory too hard? get basic battle done first
+// inventory items do things on battle field
+// must always provide one route out even if got no keys
