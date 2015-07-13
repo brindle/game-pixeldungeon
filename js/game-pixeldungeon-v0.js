a1ece19d040efb9e7e08beb4e732bc3428b2b6ba
brindle <mrbrindlestudio@gmail.com>
2015-07-13 10:55:20 +0100

    Managed to get timeout working with Hassans help. Added next level and ability to switch monster based on level selected. Need to work on using constructors.

--- js/game-pixeldungeon-v0.js
+++ js/game-pixeldungeon-v0.js
@@ -29,7 +29,7 @@
 // inventory items do things on battle field
 // must always provide one route out even if got no keys
 
-
+// BEGIN CODE
 
 // create player and set stats
 // ? constructor in future?
@@ -49,22 +49,57 @@ player.alive = function() {return player.hp > 0 };
 // create monster and set stats
 // ? constructor based on type / level?
 var monster = {};
-monster.name = "ratty";
-monster.type = "rat";
-monster.lvl = 1;
-monster.hp = 10;
-monster.sp = 10;
-monster.atk = 1;
-monster.def = 1;
-monster.spd = 1;
-monster.spc = ["bite"];
+monster.name = "";
+monster.type = "";
+monster.lvl = 0;
+monster.hp = 0;
+monster.sp = 0;
+monster.atk = 0;
+monster.def = 0;
+monster.spd = 0;
+monster.spc = [""];
 monster.inv = [];
 monster.alive = function() {return monster.hp > 0 };
 
-console.log(player);
-console.log(monster);
+// create current stage monster used in stageSetup
+var stageMonster = {};
+stageMonster.name = "";
+stageMonster.type = "";
+stageMonster.lvl = 1;
+stageMonster.hp = 10;
+stageMonster.sp = 10;
+stageMonster.atk = 1;
+stageMonster.def = 1;
+stageMonster.spd = 1;
+stageMonster.spc = [""];
+stageMonster.inv = [];
+stageMonster.alive = function() {return stageMonster.hp > 0 };
 
-var currentStage = 1;
+var monsterRat = {};
+monsterRat.name = "ratty";
+monsterRat.type = "rat";
+monsterRat.lvl = 1;
+monsterRat.hp = 5;
+monsterRat.sp = 10;
+monsterRat.atk = 1;
+monsterRat.def = 1;
+monsterRat.spd = 1;
+monsterRat.spc = ["bite"];
+monsterRat.inv = [];
+monster.alive = function() {return monsterRat.hp > 0 };
+
+var monsterGhost = {};
+monsterGhost.name = "Ghost";
+monsterGhost.type = "ghost";
+monsterGhost.lvl = 2;
+monsterGhost.hp = 10;
+monsterGhost.sp = 10;
+monsterGhost.atk = 1;
+monsterGhost.def = 1;
+monsterGhost.spd = 1;
+monsterGhost.spc = ["spook"];
+monsterGhost.inv = [];
+monster.alive = function() {return monsterGhost.hp > 0 };
 
 // get some elements we want to quickly hide and show like menus
 var spriteDisplay = $("#display");
@@ -88,78 +123,15 @@ btnPlayer1.def = $("#btn-p1-def");
 btnPlayer1.spc = $("#btn-p1-spc");
 btnPlayer1.inv = $("#btn-p1-inv");
 
-// Player1 attacks
-btnPlayer1.atk.on("click", function(){
-
-  // hide player buttons
-  hideThis(spritePlayer1Menu);
-
-  // check if player is faster than monster
-  // if player faster continue.
-
-  // if attack succeeds PLAY ANIM 
-  updateDisplay(player.name+" attacks!");
-
-  changeSprite("#player1", "attack");
-  $("#player1").animate({left: '+=150px'}, function(){
-    // play sound
-
-    // enemy hit animation
-    $("#monster1").animate({right: '-=10px'},50);
-    $("#monster1").animate({right: '+=10px'},50);
-    $("#monster1").animate({right: '-=10px'},50);
-    $("#monster1").animate({right: '+=10px'},50);
-
-    // take off atk from enemy hitpoints
-
-    $("#player1").animate({left: '-=150px'}, function(){
-      changeSprite("#player1", "default");
-      updateDisplay(monster.name+" took "+player.atk+" damage!");
-
-      // show buttons after
-      showThis(spritePlayer1Menu);
-    });
-  });
-
-  // calc new monster hp
-  var oldHP = monster.hp;
-  var damageGiven = player.atk;
-  var newHP = oldHP - damageGiven;
-  monster.hp = newHP;
-
-  // update monster stats
-  $('.monster1-hp').html( monster.hp + ' hp' );
-
-  //check monster is dead
-  if (monster.alive() === true) {
-    // run monster turn
-    // if monster health is below 3 defend
-    // if health is below 5 use special 
-  } else if (monster.alive() === false) {
-    
-    // display you won the battle message
-    
-    hideThis(spritePlayer1Menu);
-    //monster dead show animation
-
-    $("#monster1").animate({top: '+=40px'},50);
-    changeSprite("#monster1", "dead");
-    $("#monster1").animate({top: '-=40px'},50);
-    updateDisplay(player.name+" killed "+monster.name);
-    //go to next level
-  }
-  
-
-});
+var currentStage = 1;
 
 function gameSetup() {
   stageSetup(1);
   playerSetup();
-  monsterSetup();
   showThis(spriteStage);
-  updateDisplay("You encounter a RAT.")
   // startBattle();
 }
+
 
 function stageSetup(currentStage) {
 // change stage background to current stage
@@ -168,7 +140,24 @@ $("#stage").removeClass("stage").addClass("stage"+currentStage);
 soundManager.stopAll();
 // change stage background music to
 soundManager.play('stage'+currentStage);
+// setup monster relative to stage
+
+
+switch(currentStage) {
+    case 1:
+        var stageMonster = monsterRat;
+        break;
+    case 2:
+        var stageMonster = monsterGhost;
+        break;
+    default:
+        var stageMonster = monsterDefault;
+        }
+
+monsterSetup(stageMonster);
+
 }
+
 
 function playerSetup() {
   // setup player stats
@@ -182,28 +171,34 @@ function playerSetup() {
   $('#btn-p1-sp1').html( player.spc[0] );
 }
 
-function monsterSetup(monsterType, monsterLvl) {
+
+function monsterSetup(monsterName) {
+  stageMonster = monsterName;
+  console.log(monsterName);
   // update monster class on stage
-  $('#monster1').addClass('rat');
+  $('#monster1').addClass(stageMonster.type);
   // update monster stats on stage
-  $('.monster1-name').html( monster.name );
-  $('.monster1-lvl').html('lvl ' + monster.lvl );
-  $('.monster1-hp').html( monster.hp + ' hp' );
-  $('.monster1-atk').html( monster.atk + ' atk' );
-  $('.monster1-def').html( monster.def + ' def' );
-  $('#btn-m1-sp1').html( monster.spc[0] );
+  $('.monster1-name').html( stageMonster.name );
+  $('.monster1-lvl').html('lvl ' + stageMonster.lvl );
+  $('.monster1-hp').html( stageMonster.hp + ' hp' );
+  $('.monster1-atk').html( stageMonster.atk + ' atk' );
+  $('.monster1-def').html( stageMonster.def + ' def' );
+  $('#btn-m1-sp1').html( stageMonster.spc[0] );
+
+  updateDisplay("You encounter a "+stageMonster.name)
 }
+
 
 // function to hide using css class
 function hideThis(thingToHide) {
   $(thingToHide).addClass('hidden');
   console.log(thingToHide)
 }
-
 // function to remove hide using css class
 function showThis(thingToShow) {
   $(thingToShow).removeClass('hidden');
 }
+
 
 function updateDisplay(messageToDisplay) {
     // clear display
@@ -218,7 +213,8 @@ function updateDisplay(messageToDisplay) {
     };
     //Will call function in local scope
     setTimeout(displayWait, 100);
-  }
+}
+
 
   function waitFor(timeToWait) {
     var pleaseWait = function() {
@@ -229,13 +225,13 @@ function updateDisplay(messageToDisplay) {
   setTimeout(pleaseWait, timeToWait);
 }
 
+
 function changeSprite(spriteToChange, classToChange) {
   // remove all classes on player
   $(spriteToChange).removeClass()
   // change class to classToChange
   $(spriteToChange).addClass(classToChange)
 }
-
 
 
 // see if player goes first vs their speed to monster - same gives random choice
@@ -255,6 +251,83 @@ function playerPriority() {
     //             currentBattle.playerPriority = false;
     //             break;
   } 
+}
+
+
+// Player1 attacks
+btnPlayer1.atk.on("click", function(){
+
+  // hide player buttons during attack
+  hideThis(spritePlayer1Menu);
+
+  // check if player is faster than monster
+  // if player faster continue.
+
+  // play attack animation
+  updateDisplay(player.name+" attacks!");
+
+  changeSprite("#player1", "attack");
+  $("#player1").animate({left: '+=150px'}, function(){
+    // play attack sound
+
+    // enemy hit animation
+    $("#monster1").animate({right: '-=10px'},50);
+    $("#monster1").animate({right: '+=10px'},50);
+    $("#monster1").animate({right: '-=10px'},50);
+    $("#monster1").animate({right: '+=10px'},50);
+
+    // animate player back to original point + update display
+    $("#player1").animate({left: '-=150px'}, function(){
+      changeSprite("#player1", "default");
+      updateDisplay(stageMonster.name+" took "+player.atk+" damage!");
+
+      // show player buttons when animation is finished 
+      showThis(spritePlayer1Menu);
+    });
+  });
+
+  // calc new monster hp
+  var oldHP = stageMonster.hp;
+  var damageGiven = player.atk;
+  var newHP = oldHP - damageGiven;
+  stageMonster.hp = newHP;
+
+  // update monster stats
+  $('.monster1-hp').html( stageMonster.hp + ' hp' );
+
+  //check monster is dead
+  if (stageMonster.hp > 0) {
+    // run monster turn
+    // if monster health is below 3 defend
+    // if health is below 5 use special 
+  } else if (stageMonster.hp <= 0) {
+
+    // display you won the battle message
+    
+    hideThis(spritePlayer1Menu);
+    //monster dead show animation
+
+    setTimeout(monsterDead, 1000)
+
+    //go to next level
+
+    // nextStage = currentStage + 1;
+    // stageSetup(nextStage);
+    setTimeout(nextStage, 3000)
+
+  }
+});
+
+var monsterDead = function(){
+  $("#monster1").animate({top: '+=40px'},50);
+  changeSprite("#monster1", "dead");
+  $("#monster1").animate({top: '-=40px'},50);
+  updateDisplay(player.name+" killed "+stageMonster.name);
+}
+
+var nextStage = function(){
+  nextStageNo = currentStage + 1;
+  stageSetup(nextStageNo);
 }
 
 // setup sound - run game after all sound files are loaded
